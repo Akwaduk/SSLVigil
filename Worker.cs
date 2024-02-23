1bd0d2c0237252324b24eaf21e514d441237ca40
Erik Johnson <akwaduk13@gmail.com>
2024-02-22 20:40:11 -0600

    Initial commit

---
+++ Worker.cs
@@ -0,0 +1,23 @@
+namespace SSLVigil;
+
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> _logger;
+
+    public Worker(ILogger<Worker> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            if (_logger.IsEnabled(LogLevel.Information))
+            {
+                _logger.LogInformation("SSLVigil Worker running at: {time}", DateTimeOffset.Now);
+            }
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
